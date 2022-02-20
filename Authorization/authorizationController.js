742c2ba9b57d7b322a7cc6e3720f268ddcb733c8
NikitaKissa <mkissa08@gmail.com>
2022-02-20 22:54:09 +0200

    bad animations

--- Authorization/authorizationController.js
+++ Authorization/authorizationController.js
@@ -1,4 +1,8 @@
 $(document).ready(function (){
+    $("#email-field").on("change", function (){
+        $("#email-text").animate("font-size: 13px", "fast");
+    });
+
     $("#send-button").click(function (){
 
     });
