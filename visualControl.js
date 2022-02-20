efe3b1422790932f4b5142e87a6f195e377b8195
NikitaKissa <mkissa08@gmail.com>
2022-02-20 21:18:14 +0200

    add button

--- visualControl.js
+++ visualControl.js
@@ -1,4 +1,9 @@
 $(document).ready(function (){
     const windowHeight = $(window).height();
     $("#names-panel").css({height: windowHeight}); //set height to names panel
+
+    for (let i = 2; i <= 12; i++){
+        $("#class-select-area").append(`<option>${i}</option>`); // adding classes to <select>
+    }
+
 });
