a87dd3ea11de5d882a1ecfb32b11ed647019b9c6
NikitaKissa <mkissa08@gmail.com>
2022-04-18 19:50:42 +0300

    commit pl

--- visualControl.js
+++ visualControl.js
@@ -2,8 +2,11 @@ $(document).ready(function (){
     const windowHeight = $(window).height();
     $("#names-panel").css({height: windowHeight}); //set height to names panel
 
-    for (let i = 2; i <= 12; i++){
+    for (let i = 2; i <= 10; i++){
         $("#class-select-area").append(`<option>${i}</option>`); // adding classes to <select>
     }
 
+    $("#send-button").click(function (){
+
+    });
 });
